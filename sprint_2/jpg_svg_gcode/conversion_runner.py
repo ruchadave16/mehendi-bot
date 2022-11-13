f10ad72491a2ff1ed789ad1a17a25f155a7fabb9
Emma Mascillaro <emascillaro@gmail.com>
2022-11-13 02:41:57 -0500

    JPG to SVG converter transparency error

--- sprint_2/jpg_svg_gcode/conversion_runner.py
+++ sprint_2/jpg_svg_gcode/conversion_runner.py
@@ -1,6 +1,6 @@
 from jpg_svg import jpg_svg_converter
 from svg_gcode import svg_gcode_converter
 
-jpg_file = ["Cat03.jpg"]
+jpg_file = ["circle.jpg"]
 svg_file = jpg_svg_converter(jpg_file)
 gcode_file = svg_gcode_converter(svg_file)
--- sprint_2/jpg_svg_gcode/test.py
+++ sprint_2/jpg_svg_gcode/test.py
@@ -6,8 +6,8 @@ from svg_to_gcode.compiler import Compiler, interfaces
 # how far down the tool moves after every pass. Set it to 0 if your machine does not support Z axis movement.
 gcode_compiler = Compiler(interfaces.Gcode, movement_speed=1000, cutting_speed=300, pass_depth=5)
 
-curves = parse_file("current.svg") # Parse an svg file into geometric curves
+curves = parse_file("circle.svg") # Parse an svg file into geometric curves
 
 gcode_compiler.append_curves(curves) 
-gcode_compiler.compile_to_file("current.gcode", passes=2)
+gcode_compiler.compile_to_file("circle.gcode", passes=2)
     
