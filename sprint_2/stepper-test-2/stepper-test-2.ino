ef6dba8b4d02f7033f25fd012b7187fdcf03fc03
Cherry Pham <meokkmun@gmail.com>
2022-11-15 14:20:55 -0500

    Add adjusted motor code

---
+++ sprint_2/stepper-test-2/stepper-test-2.ino
@@ -0,0 +1,37 @@
+#include <Stepper.h>
+
+// Define motors
+Stepper stepperY1(200, 4, 5);
+Stepper stepperY2(200, 6, 7);
+Stepper stepperX(200, 2, 3);
+
+String curr_line;
+float feedrate = 0;
+float step_delay = 0;
+
+// Define current position of ink
+float x_pos;
+float y_pos;
+float reset;
+
+/**
+  * Provide basic description of project and what each function does
+  */  
+void help() {
+  Serial.println("Mehendi Bot");
+}
+
+void setup() {
+  Serial.begin(9600);
+  stepperX.setSpeed(1000);
+  stepperY1.setSpeed(500);
+  stepperY2.setSpeed(500);
+  help();
+}
+
+
+void loop() {
+  stepperX.step(1);
+  stepperY1.step(1);
+  stepperY2.step(1);
+}
