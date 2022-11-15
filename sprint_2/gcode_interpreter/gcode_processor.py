6ab92125e8b4ea9ef937ab1635a03432969ecd7b
Emma Mascillaro <emascillaro@gmail.com>
2022-11-15 13:08:09 -0500

    process gcode

---
+++ sprint_2/gcode_interpreter/gcode_processor.py
@@ -0,0 +1,12 @@
+from serial import Serial
+import time
+
+def separate_lines(gcode_file):
+    return gcode_file.readlines()
+
+def write_to_serial(gcode_array):
+
+    ser = Serial('/dev/ttyACM0', 115200, timeout=0.050)
+
+    for command in gcode_array:
+        ser.write(command)
