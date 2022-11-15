8b11ddb77842c69d43eb6469be1c8795c3f199ec
Cherry Pham <meokkmun@gmail.com>
2022-11-15 13:19:18 -0500

    Organize and update motor codes

--- sprint_2/stepper-motor-testing/stepper-motor-testing.ino
+++ sprint_2/stepper-motor-testing/stepper-motor-testing.ino
@@ -8,11 +8,13 @@
  **/
 
 // Define pin connections & motor's steps per revolution
-const int motorLeft_dir = 5;
-const int motorLeft_step = 6;
-const int motorRight_dir = 3;
-const int motorRight_step = 4;
-const int stepsPerRev = 200;
+const int motorCenter_dir = 2;
+const int motorCenter_step = 3;
+const int motorLeft_dir = 4;
+const int motorLeft_step = 5;
+const int motorRight_dir = 6;
+const int motorRight_step = 7;
+const int stepsPerRev = 24000;
 
 
 // Set motor commands to loop over
@@ -30,22 +32,28 @@ void setup()
   Serial.begin(9600);
 
   // Declare pins as Outputs
+  pinMode(motorCenter_step, OUTPUT);
+  pinMode(motorCenter_dir, OUTPUT);
   pinMode(motorLeft_step, OUTPUT);
   pinMode(motorLeft_dir, OUTPUT);
   pinMode(motorRight_step, OUTPUT);
   pinMode(motorRight_dir, OUTPUT);
 }
-
+// HIGH - CLOCKWISE - toward motor
+// LOW - ANTICLOCKWISE - away fr motor
 void loop()
 {
-  digitalWrite(motorLeft_dir, HIGH);
-  digitalWrite(motorRight_dir, HIGH);
+  digitalWrite(motorCenter_dir, LOW);
+  digitalWrite(motorLeft_dir, LOW);
+  digitalWrite(motorRight_dir, LOW);
   // Spin motor slowly
   for(int x = 0; x < stepsPerRev; x++)
   {
     digitalWrite(motorLeft_step, HIGH);
     digitalWrite(motorRight_step, HIGH);
+    digitalWrite(motorCenter_step, HIGH);
     delayMicroseconds(1000);
+    digitalWrite(motorCenter_step, LOW);
     digitalWrite(motorLeft_step, LOW);
     digitalWrite(motorRight_step, LOW);
     delayMicroseconds(1000);
