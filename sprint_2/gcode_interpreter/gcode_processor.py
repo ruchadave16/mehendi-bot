e4dab2a6cce49eeec7221140163cb9b99288c5fd
Rucha Dave <ruchadave16@gmail.com>
2022-12-01 16:45:38 -0500

    Fix serial communication

--- sprint_2/gcode_interpreter/gcode_processor.py
+++ sprint_2/gcode_interpreter/gcode_processor.py
@@ -1,39 +1,50 @@
 from serial import Serial
 import time
 
-arduino = Serial(port='/dev/ttyACM0', baudrate=115200, timeout=.1)
+arduino = Serial(port='/dev/ttyACM1', baudrate=115200, timeout=.1)
 
 def fetch_data():
-    data = arduino.readline().decode()
-    return data
+    return arduino.readline().decode()
 
 null_list = []
 
 def write_read(x):
     check_ready = ''
-    while check_ready != "Next":
-        print(f'"{check_ready}"')
+    while "Next" not in check_ready:
         check_ready = fetch_data()
+        print(check_ready)
+    print("DONE")
 
+    time.sleep(1.0)
     arduino.write(bytes(x, 'utf-8'))
     print(f"Sent: {x}")
-    time.sleep(0.5)
+    time.sleep(1.0)
     data = fetch_data()
-
-    while (len(data) == 0):
-        print("In Null List")
+    if data == "\n":
         data = fetch_data()
-    print(f"Data is: '{data}'")
-    return data
-        # print(data)
-        # while data != "done":
-        #     time.sleep(0.5)
-        # return data
+    return(data)
+
+    
+
+    
+    # time.sleep(0.5)
+    # data = fetch_data()
+
+    # # while (len(data) == 0):
+    # # data = fetch_data()
+    # # print(data)
+    # print(f"Data is: '{data}'")
+    # return data
+    # #     # print(data)
+    # #     # while data != "done":
+    # #     #     time.sleep(0.5)
+    # #     # return data
 
 def separate_lines(gcode_file):
     return gcode_file.readlines()
 
 def write_to_serial(gcode_array):
+    gcode_array = ["", "", "", "", "", "", ""] + gcode_array
     for command in gcode_array:
         answer = write_read(command)
-        # print(f"Received: {answer}")
+        print(f"Received: '{answer}'")
